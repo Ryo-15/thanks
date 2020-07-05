bb4f35d115ace87313defb877205e970aeadf401
Ryo-15 <tomorrows.way15@gmail.com>
2020-07-05 20:37:08 +0900

    [add]controller

---
+++ app/controllers/admins/departments_controller.rb
@@ -0,0 +1,2 @@
+class Admins::DepartmentsController < ApplicationController
+end
---
+++ test/controllers/admins/departments_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Admins::DepartmentsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
