af9d4d324cd31cf5aa0be147b69b4757a9fb8a2d
Ryo-15 <tomorrows.way15@gmail.com>
2020-07-26 21:35:29 +0900

    [fix]chart

--- app/controllers/admins/homes_controller.rb
+++ app/controllers/admins/homes_controller.rb
@@ -63,11 +63,11 @@ class Admins::HomesController < ApplicationController
               .limit(20)
               .pluck(:user_id)
     )
-    # hoge = Post.group(:receiver_id).count
-    # @hoge = []
-    # hoge.each do|h|
-    #   @hoge << [User.find(h[0]).name,h[1]]
-    # end
+    # receive = Post.group(:receiver_id).count
+    # @recieve = []
+    # receive.each do|r|
+    #   @receive << [User.find(r[0]).full_name,r[1]]
+    # # end
     # @posts = Post.left_joins(:favorites).group('posts.id').order('COUNT(favorites.id) DESC')
   end
 end
