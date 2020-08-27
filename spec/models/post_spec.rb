3f49fa1b97bb05f9e5702f3d55d97e0534b04a89
Ryo-15 <tomorrows.way15@gmail.com>
2020-08-27 19:59:46 +0900

    [update]rspec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -49,6 +49,11 @@ RSpec.describe Post, type: :model do
         expect(Post.reflect_on_association(:user).macro).to eq :belongs_to
       end
     end
+    context 'PostCommentモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(Post.reflect_on_association(:post_comment).macro).to eq :has_many
+      end
+    end
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -50,10 +50,44 @@ RSpec.describe User, type: :model do
       end
     end
 
+    context 'passwordカラム' do
+      let(:test_user) { user }
+
+      it '空欄でないこと' do
+        test_user.password = ''
+        is_expected.to eq false
+      end
+      it '空欄の場合はエラーが出る' do
+        test_user.password = ''
+        test_user.valid?
+        expect(test_user.errors[:password]).to include("を入力してください")
+      end
+      it '6文字以上であること' do
+        test_user.password = Faker::Lorem.characters(number: 1)
+        is_expected.to eq false
+      end
+      it '６文字未満の場合はエラーが出る' do
+        test_user.password = Faker::Lorem.characters(number: 1)
+        test_user.valid?
+        expect(test_user.errors[:password]).to include("は6文字以上で入力してください")
+      end
+      it 'パスワードが不一致' do
+        test_user.password = "password1"
+        test_user.password_confirmation = "password2"
+        test_user.valid?
+        expect(test_user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません")
+      end
+    end
+
     describe 'アソシエーションのテスト' do
       context 'Postモデルとの関係' do
         it '1:Nとなっている' do
-          expect(User.reflect_on_association(:post).macro).to eq :belongs_to
+          expect(User.reflect_on_association(:post).macro).to eq :has_many
+        end
+      end
+      context 'PostCommentモデルとの関係' do
+        it '1:Nとなっている' do
+          expect(User.reflect_on_association(:post_comment).macro).to eq :has_many
         end
       end
     end
