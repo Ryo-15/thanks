f30e02a3f3a0473131d59905a27ed35be8b4d103
Ryo-15 <tomorrows.way15@gmail.com>
2020-08-28 16:36:32 +0900

    [update]rspec

---
+++ spec/models/favorite_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe Favorite, type: :model do
+  describe 'アソシエーションのテスト' do
+    let(:association) do
+      described_class.reflect_on_association(target)
+    end
+
+    context 'Userモデルとの関係' do
+      let(:target) { :user }
+
+      it 'N:1となっている' do
+        expect(association.macro).to eq :belongs_to
+      end
+    end
+
+    context 'Postモデルとの関係' do
+      let(:target) { :post }
+
+      it 'N:1となっている' do
+        expect(association.macro).to eq :belongs_to
+      end
+    end
+
+  end
+end
