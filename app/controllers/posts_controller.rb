8184828b9622709b5f9fc65d450b38b6f137cd1a
Ryo-15 <tomorrows.way15@gmail.com>
2020-08-29 16:53:19 +0900

    [update]rubocop

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,9 @@ class PostsController < ApplicationController
     @post = Post.new
     @search = User.ransack(params[:q])
     @users = @search.result(distinct: true)
+    
+    binding.pry
+    
   end
 
   def create
